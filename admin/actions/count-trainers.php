f08823aa481fc288b14828ba23413e16bbd5bda6
bivekmehta9 <bivekmehta9@gmail.com>
2024-10-27 08:53:50 +0545

    commit message

---
+++ admin/actions/count-trainers.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername="localhost";
+$uname="root";
+$pass="i1love2my3brother@";
+$db="gymnsb";
+
+$conn=mysqli_connect($servername,$uname,$pass,$db);
+
+if(!$conn){
+    die("Connection Failed");
+}
+
+$sql = "SELECT * FROM staffs WHERE designation='Trainer'";
+                $query = $conn->query($sql);
+
+                echo "$query->num_rows";
+?><!-- Visit codeastro.com for more projects -->
